8b7216ed47eedfa8acd69d02e564b80d6cfee907
minhdang0603 <ruabin1163@gmail.com>
2024-06-10 20:18:42 +0700

    first commit

---
+++ DataAccessObject/BaseDAL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessObject
+{
+    public class BaseDAL
+    {
+        public StockDataProvider StockDataProvider { get; set; } = null;
+        public SqlConnection connection = null;
+        public BaseDAL() 
+        {
+            var connectionString = "Server=TAMINHDANG\\MINHDANG;uid=sa;pwd=1163ruabin;database=MyStore;TrustServerCertificate=true";
+            StockDataProvider = new StockDataProvider(connectionString);
+        }
+        //------
+        public void CloseConnection() => StockDataProvider.CloseConnection();
+
+    }
+}
